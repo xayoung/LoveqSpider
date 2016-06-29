77e4c9117b7d108521bd2b168f855ad06fb87d04
xayoung <xayoung@hotmail.com>
2016-06-29 14:36:07 +0800

    fix upload

--- put.py
+++ put.py
@@ -2,7 +2,15 @@
 import pycurl
 import json
 import requests
+import xinge
+import time
 
+
+loaclYear = time.strftime("%Y", time.localtime())
+loaclMonth = time.strftime("%m", time.localtime())
+
+# iOS全量推送
+#print xinge.PushAllIos(2200205039, '284ce032cf63af6633ad743b6cdd13c4', 'python自动推送测试8:56', xinge.XingeApp.ENV_PROD)
 
 f = file("06JSONFile.json")
 s = json.load(f)
@@ -11,22 +19,10 @@ f.close
 
 
 
-url = 'https://pythontest.wilddogio.com/test.json'
-json_data = s
-#headers = {'X-Api-Key': 'asdf1234'}
+url = 'https://pythontest.wilddogio.com/'+ loaclYear + '/' + loaclYear+loaclMonth +'.json'
+#json_data = s
+#r = requests.request('PUT', url, json=json_data)
+array = json.dumps(requests.get(url).json())
+data = json.loads(array)
+print(len(data))
 
-r = requests.request('PUT', url, json=json_data)
-print(r.json)
-
-
-#c = pycurl.Curl()
-#c.setopt(pycurl.URL, "https://pythontest.wilddogio.com/test.json/")
-#c.setopt(pycurl.HTTPHEADER, ['Content-Type: application/json', 'Accept: application/json'])
-#c.setopt(pycurl.CUSTOMREQUEST, "PUT")
-#data = json.dumps({"xayoung":"001"})
-#c.setopt(pycurl.PUT, True)
-#c.setopt(pycurl.POSTFIELDS,data)
-#c.perform()
-#c.close()
-
-#curl -X PUT -d '{ "alanisawesome": { "name": "Alan Turing", "birthday": "June 23, 1912" } }' 'https://pythontest.wilddogio.com/test.json'
--- update.py
+++ update.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 import main
+import requests
 import json
 
-#更新单个月份,输入页码
+#输入页码,爬取节目信息
 spider = main.Spider()
 spider.savePagesInfo(1,2)
+#分类工具进行筛选
 tool = main.Tools()
 totalJSON = {}
-tool.store('06', tool.saveSingleMonthJSON('2016',6,spider.JSONMP3))
+#upload至wilddog后台
+wilddogURL = 'https://pythontest.wilddogio.com/'
+
+uploadTool = main.uploadPushData()
+
+newJSON = tool.saveSingleMonthJSON(uploadTool.loaclYear,uploadTool.loaclMonth,spider.JSONMP3)
+newJSOND = json.dumps(newJSON)
+newJSONdata = json.loads(newJSOND)
+print newJSONdata
+
+url = wilddogURL+ uploadTool.loaclYear + '/' + uploadTool.loaclYear+uploadTool.loaclMonth +'.json'
+oldJSON = json.dumps(requests.get(url).json())
+oldJSONdata = json.loads(oldJSON)
+print oldJSONdata
+
+if  oldJSONdata == None:
+    uploadTool.uploadJSON(wilddogURL,newJSON)
+else:
+    if len(newJSONdata) > len(oldJSONdata):
+        uploadTool.uploadJSON(wilddogURL, newJSON)
+    else:
+        print '无更新'
+#写入文件
+#tool.store('06', tool.saveSingleMonthJSON('2016',6,spider.JSONMP3))
