54610faf70f9e687b43ccf89130f662dc97c4fb9
xayoung <xayoung@hotmail.com>
2016-09-04 22:57:57 +0800

    change message

--- update.py
+++ update.py
@@ -11,28 +11,36 @@ spider.savePagesInfo(1,2)
 #分类工具进行筛选
 tool = main.Tools()
 totalJSON = {}
-#upload至wilddog后台
-wilddogURL = 'https://xxx.xxxx.xxxx'
+#wilddog后台
+wilddogURL = 'https://loveq.wilddogio.com/program/'
 
 uploadTool = main.uploadPushData()
 newJSON = tool.saveSingleMonthJSON(uploadTool.loaclYear,uploadTool.loaclMonth,spider.JSONMP3)
 newJSOND = json.dumps(newJSON)
 newJSONdata = json.loads(newJSOND)
-#print newJSONdata
+print newJSONdata
 
+#读取老数据
 url = wilddogURL+ uploadTool.loaclYear + '/' + uploadTool.loaclYear+uploadTool.loaclMonth +'.json'
+print url
 oldJSON = json.dumps(requests.get(url).json())
 oldJSONdata = json.loads(oldJSON)
-#print oldJSONdata
+print oldJSONdata
 
+#如果老数据为空
 if  oldJSONdata == None:
-    uploadTool.uploadJSON(wilddogURL,newJSON)
-    xinge.PushAllIos(xxx, 'xxx', '最新节目已更新,快去下载吧!', xinge.XingeApp.ENV_PROD)
-    print 'newMonthDataToWilddog'
+    #上传新数据开辟节点,新年份/新月份,必须有数据再上传
+    if len(newJSON) > 0 :
+        uploadTool.uploadJSON(wilddogURL,newJSON)
+        xinge.PushAllIos(2200205039, 'I3327UDVYP4J', '最新节目已更新,快去下载吧!', xinge.XingeApp.ENV_PROD)
+        print 'newDataToWilddog'
+    else:
+        print('this is a new month/year,but nothing update')
 else:
+    #有老数据,但是需要更新,比对新老数据的个数
     if len(newJSONdata) > len(oldJSONdata):
         uploadTool.uploadJSON(wilddogURL, newJSON)
-        xinge.PushAllIos(xxx, 'xxxx', '最新节目已更新,快去下载吧!', xinge.XingeApp.ENV_PROD)
+        xinge.PushAllIos(2200205039, 'I3327UDVYP4J', '最新节目已更新,快去下载吧!', xinge.XingeApp.ENV_PROD)
         print 'updateDataToWilddog'
     else:
         print 'none update'
